bb573f1617ce81aa1051bbd0f86cded18abcf8af
ToshianaW <toshianawilliams@gmail.com>
2024-05-20 12:52:28 -0500

    install tailwind

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,9 +1,12 @@
 // noinspection JSIgnoredPromiseFromCall
-
+import '../css/app.css'
 import './bootstrap';
 
 import { createApp, h, type DefineComponent } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+
+import PrimeVue from "primevue/config";
+import Aura from 'primevue/themes/aura';
 
 createInertiaApp({
     resolve: name => {
@@ -13,6 +16,11 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
             .mount(el)
     },
 });
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserRegistrationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function() {
     return Inertia::render('Index/Index');
 });
+
+Route::get('/register', [UserRegistrationController::class, 'create']);
