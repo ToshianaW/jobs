75c9318516f1dce2a118ff4ccf02d6fb3d1ea167
ToshianaW <toshianawilliams@gmail.com>
2024-05-20 15:45:09 -0500

    login/logout

---
+++ resources/js/Pages/Auth/UserLogin.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { useForm } from '@inertiajs/vue3';
+import Panel from 'primevue/panel';
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+
+const form = useForm({
+  email: null,
+  password: null,
+})
+</script>
+
+<template>
+  <div class="grid h-screen place-content-center">
+    <Panel>
+      <form @submit.prevent="form.post('/login')">
+        <div class="flex flex-col gap-1">
+          <label
+              for="email"
+          >
+            Email
+          </label>
+          <InputText
+              type="email"
+              v-model="form.email"
+              id="email"
+          />
+          <small v-if="form.errors.email" class="text-red-500">
+            {{ form.errors.email }}
+          </small>
+        </div>
+        <div class="flex flex-col gap-1">
+          <label
+              for="password"
+          >
+            Password
+          </label>
+          <InputText
+              type="password"
+              v-model="form.password"
+              id="first_name"
+          />
+          <small v-if="form.errors.password" class="text-red-500">
+            {{ form.errors.password }}
+          </small>
+        </div>
+        <div class="flex justify-end mt-3">
+          <Button type="submit" label="Login" />
+        </div>
+      </form>
+    </Panel>
+  </div>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use App\Http\Controllers\UserRegistrationController;
+use App\Http\Controllers\UserSessionController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function() {
     return Inertia::render('Index/Index');
-})->name('home');
+})->name('home')->middleware('auth');
 
 Route::get('/register', [UserRegistrationController::class, 'create']);
 Route::post('/register', [UserRegistrationController::class, 'store']);
+
+Route::get('/login', [UserSessionController::class, 'create'])->name('login');
+Route::post('/login', [UserSessionController::class, 'store']);
+
+Route::post('/logout', [UserSessionController::class, 'destroy'])->name('logout');
