5509eedbcae19b8cd74afc45fc1fbdbef87e7fd2
ToshianaW <toshianawilliams@gmail.com>
2024-05-25 11:21:34 -0500

    Create Mailer

---
+++ app/Mail/EnquiryNotification.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Listing;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class EnquiryNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public Listing $listing;
+    public string $enquirerEmail;
+
+    /**
+     * Create a new message instance.
+     *
+     * @param Listing $listing
+     * @param string $enquirerEmail
+     */
+    public function __construct(Listing $listing, string $enquirerEmail)
+    {
+        $this->listing = $listing;
+        $this->enquirerEmail = $enquirerEmail;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.enquiry')
+            ->with([
+                'listingTitle' => $this->listing->title,
+                'enquirerEmail' => $this->enquirerEmail,
+            ]);
+    }
+}
